d73ffc5384e16e1ce0cbce7d019d6e0a63e68c15
arturgorski14 <arturgorski14@gmail.com>
2024-09-08 10:31:45 +0200

    test

--- tests/test_group_files_by_date.py
+++ tests/test_group_files_by_date.py
@@ -10,38 +10,41 @@ from group_files_by_date import GroupingLevel, group_files_by_date
             [
                 "IMG_20230901_something.jpg",
                 "VID_20240902_something.mp4",
+                "IMG_20241001_something.jpg",
                 "PANO_20241001_something.jpg",
             ],
             GroupingLevel.YYYY,
             {
                 "2023": ["IMG_20230901_something.jpg"],
-                "2024": ["VID_20240902_something.mp4", "PANO_20241001_something.jpg"],
+                "2024": ["VID_20240902_something.mp4", "IMG_20241001_something.jpg", "PANO_20241001_something.jpg"],
             },
         ),
         (
             [
                 "IMG_20230901_something.jpg",
                 "VID_20240902_something.mp4",
+                "IMG_20241001_something.jpg",
                 "PANO_20241001_something.jpg",
             ],
             GroupingLevel.YYYYMM,
             {
                 "2023.09": ["IMG_20230901_something.jpg"],
                 "2024.09": ["VID_20240902_something.mp4"],
-                "2024.10": ["PANO_20241001_something.jpg"],
+                "2024.10": ["IMG_20241001_something.jpg", "PANO_20241001_something.jpg"],
             },
         ),
         (
             [
                 "IMG_20230901_something.jpg",
                 "VID_20240902_something.mp4",
+                "IMG_20241001_something.jpg",
                 "PANO_20241001_something.jpg",
             ],
             GroupingLevel.YYYYMMDD,
             {
                 "2023.09.01": ["IMG_20230901_something.jpg"],
                 "2024.09.02": ["VID_20240902_something.mp4"],
-                "2024.10.01": ["PANO_20241001_something.jpg"],
+                "2024.10.01": ["IMG_20241001_something.jpg", "PANO_20241001_something.jpg"],
             },
         ),
     ],
