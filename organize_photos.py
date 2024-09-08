ceb0aa3bdbd43f87dc4db2c6bbed43e9b36cc881
arturgorski14 <arturgorski14@gmail.com>
2024-09-08 10:00:17 +0200

    refactor: move_files

--- organize_photos.py
+++ organize_photos.py
@@ -1,11 +1,13 @@
 import logging
+import os
+import shutil
 import tkinter as tk
 from pathlib import Path
 from tkinter import messagebox, ttk
+from typing import Dict, List, Tuple
 
 from get_matching_files import get_matching_files
 from group_files_by_date import GroupingLevel, group_files_by_date
-from move_files import move_files
 from select_folder import select_folder
 
 
@@ -149,13 +151,51 @@ class OrganizePhotos(tk.Toplevel):
             f"Liczba nowych folderów: {len(grouped_files)}\nI ich nazwy: {grouped_files.keys()}"
         )
 
-        successes, failures = move_files(grouped_files, Path(folder_path))
+        successes, failures = self.move_files(grouped_files, Path(folder_path))
         self.progress_bar.step(33)
         self.__update_label(f"Przeniesiono {successes} z {successes + failures} plików")
 
-        self.display_message_box(successes, failures)
+        self.__display_message_box(successes, failures)
 
-    def display_message_box(self, successes: int = 20, failures: int = 5) -> None:
+    def move_files(
+        self, grouped_files: Dict[str, List[str]], root_folder: Path
+    ) -> Tuple[int, int]:
+        """
+        Moves files into their corresponding folders based on the grouping.
+
+        Parameters:
+        - grouped_files (dict): A dictionary where the key is the folder name and the value is a list of file paths.
+        - destination_folder (str): The root folder where grouped folders will be created.
+
+        :returns
+        Number of successfully and unsuccessfully moved files.
+
+        Example structure of `grouped_files`:
+        {
+            '2023-09-01': ['file1.jpg', 'file2.jpg'],
+            '2023-09-02': ['file3.jpg']
+        }
+        """
+        successes = 0
+        failures = 0
+        for folder_name, files in grouped_files.items():
+            # Create the destination directory if it doesn't exist
+            target_folder = os.path.join(root_folder, folder_name)
+            os.makedirs(target_folder, exist_ok=True)
+            for file_path in files:
+                try:
+                    source_path = os.path.join(root_folder, file_path)
+                    # Move the file to the target folder
+                    shutil.move(source_path, target_folder)
+                    successes += 1
+                    logging.info(f"Moved {file_path} to {target_folder}")
+                except Exception as e:
+                    failures += 1
+                    logging.warning(f"Failed to move {file_path}: {e}")
+
+        return successes, failures
+
+    def __display_message_box(self, successes: int = 20, failures: int = 5) -> None:
         title = "Przetwarzanie zakończone"
         msg = f"Przeniesiono {successes} z {successes + failures} plików"
         if not failures:
--- tests/test_move_files.py
+++ tests/test_move_files.py
@@ -1,8 +1,10 @@
 from pathlib import Path
+from unittest.mock import MagicMock
 
 import pytest
 
-from move_files import move_files
+from group_files_by_date import GroupingLevel
+from organize_photos import OrganizePhotos
 
 
 @pytest.fixture
@@ -12,7 +14,6 @@ def setup_files(tmp_path):
     """
     files = [tmp_path / "file1.txt", tmp_path / "file2.txt", tmp_path / "file3.txt"]
 
-    # Create files in the temporary directory
     for file in files:
         file.touch()
 
@@ -25,27 +26,41 @@ def setup_files(tmp_path):
     return grouped_files, tmp_path
 
 
-def test_move_files(setup_files):
-    grouped_files, tmp_path = setup_files
+class TestMoveFiles:
+    @pytest.fixture
+    def setup_app(self, tmp_path):
+        # Create an instance of OrganizePhotos with dummy folder_path and grouping level
+        app = OrganizePhotos(
+            str(tmp_path), grouping_level=GroupingLevel.YYYY
+        )
 
-    # Destination folder for the test
-    destination_folder = tmp_path / "destination"
-    destination_folder.mkdir()
+        # Mock GUI components
+        app.progress_bar = MagicMock()
+        app.progress_bar_label = MagicMock()
+        return app
 
-    # Call the move_files function
-    move_files(grouped_files, destination_folder)
+    def test_move_files(self, setup_app, setup_files):
+        app = setup_app
+        grouped_files, tmp_path = setup_files
 
-    # Verify that the files were moved to the correct locations
-    for group, files in grouped_files.items():
-        target_folder = destination_folder / group
-        for file_path in files:
-            assert (
-                target_folder / Path(file_path).name
-            ).exists(), f"{file_path} not found in {target_folder}"
+        # Destination folder for the test
+        destination_folder = tmp_path / "destination"
+        destination_folder.mkdir()
 
-    # Ensure original files are not in the source folder anymore
-    for files in grouped_files.values():
-        for file_path in files:
-            assert not Path(
-                file_path
-            ).exists(), f"{file_path} still exists in the original location"
+        # Call the move_files function
+        app.move_files(grouped_files, destination_folder)
+
+        # Verify that the files were moved to the correct locations
+        for group, files in grouped_files.items():
+            target_folder = destination_folder / group
+            for file_path in files:
+                assert (
+                    target_folder / Path(file_path).name
+                ).exists(), f"{file_path} not found in {target_folder}"
+
+        # Ensure original files are not in the source folder anymore
+        for files in grouped_files.values():
+            for file_path in files:
+                assert not Path(
+                    file_path
+                ).exists(), f"{file_path} still exists in the original location"
