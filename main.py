b3b577c70c0bdb18409a0527f242f8eb44b5655b
Artur Górski <37508713+arturgorski14@users.noreply.github.com>
2024-10-06 11:08:49 +0200

    feat: SettingsManager (#9)
    * feat: customizable configuration
    
    * feat: settings manager
    
    * remove redundant comment
    
    * linters
    
    * tests

--- main.py
+++ main.py
@@ -1,38 +1,13 @@
-import logging
 import configparser
-import os.path
+import logging
 
-from constants import GroupingLevel
 from main_window import MainWindow
-
-
-def load_configuration(filename: str = "config.ini") -> str:
-    config = configparser.ConfigParser()
-    if not os.path.exists(filename):
-        config['DEFAULT'] = {'grouping_level': '1'}
-        with open(filename, "w") as config_file:
-            config.write(config_file)
-
-    fn = config.read(filename)
-    assert fn, f"{fn=}"
-    grouping_level = config["DEFAULT"].get("grouping_level", "0")
-    try:
-        grouping_level = int(grouping_level)
-        mapping = {
-            1: GroupingLevel.YYYY.value,
-            2: GroupingLevel.YYYYMM.value,
-            3: GroupingLevel.YYYYMMDD.value,
-        }
-        return mapping[grouping_level]
-    except (ValueError, KeyError):
-        logging.warning(f"Please remove {filename} file (it will be restored to defaults).")
-        return GroupingLevel.YYYY.value
+from settings_manager import SettingsManager
 
 
 def main():
-    grouping_level: str = load_configuration()
-    logging.info(f"{grouping_level=}")
-    app = MainWindow(grouping_level)  # TODO: MainWindow doesn't care. OrganizePhotos should be initialized with this.
+    settings_manager = SettingsManager(configparser.ConfigParser())
+    app = MainWindow(settings_manager)
     app.mainloop()
 
 
